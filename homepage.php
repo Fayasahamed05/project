b720e67a50c5fc94e78b0fc0a0271353d54a91cb
Mohamed Habzar <132914523+MohamedHabzar@users.noreply.github.com>
2023-11-20 15:55:20 +0530

    upload

---
+++ homepage.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <title>Homepage</title>
+    <link rel="stylesheet" href="./homepage.css">
+    <link rel="stylesheet" href="./bootstrap-5.0.2-dist/css/bootstrap.min.css">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+</head>
+<body>
+                <div class="university-img">
+                    <div class="img-content">
+                       <h1 class="firstimg-content"> UNIVERSITY IS HIGHLY RANKED YEAR AFTER YEAR AS ONE<br/> OF THE NATION'S TOP CHRISTIAN UNIVERSITIES</h1>
+                    </div>
+                    <div class="img-content2">
+                        <p>Learn. Thrive. Succeed.<br/>Begin your journey today!</p>
+                    </div>
+                    <div class="threebox">
+                        <div class="undergraduate">
+                            <a href="./top-ug.php"><button class="a-undergrade">UNDERGRADUATE</button></a>
+                        </div>
+                        <div class="graduate">
+                            <a href="./top-pg.php"><button class="a-graduate">POSTGRADUATE</button></a>
+                        </div>
+                        <div class="online">
+                            <a href="./online-programs.php"><button class="a-online">ONLINE</button></a>
+                        </div>
+                    </div>
+                </div>
+            <div class="journey-btn">
+                <div class="journey">
+                     <h1>BEGIN YOUR JOURNEY</h1>
+                </div>
+                <div class="btn-div">
+                    <a href="./application.php"><button class="apply-btn">APPLY NOW</button></a>
+                </div>
+            </div>
+           
+            <div class="explore-img">
+                <div class="community-content" data-aos="zoom-out-right">
+                    <h2 class="community-h2">EXPLORE EXCITING<br/>PROGRAMS</h2>
+                    <p class="community-content2">Live what you love. More than 100 programs of study integrate top-tier academics with Christian faith. What's God's calling for your life?</p>
+                </div>  
+            </div>
+
+            <div class="visit-main">
+                <div class="come-visit">
+                    <h1>COME FOR A VISIT</h1>
+                    <p>One day is all it takes. Talk to professors and students. Attend class. Eat at Brewer and hang out in the Bowld. Don't just imagine your life here, come experience it for yourself.</p>
+                </div> 
+            </div>
+            
+            <div class="community-img">
+                <div class="community-content" data-aos="zoom-out-right">
+                    <h2 class="community-h2">ENGAGING CAMPUS<br/>COMMUNITY</h2>
+                    <p class="community-content2">University is a welcoming, accessible and safe place to live and learn. Our campus housing has been ranked third in the nation, with a private bedroom for every student and a washer/dryer in every apartment. Ninety student organizations, NCAA DII sports, concerts and residence life activities mean you'll be involved from your very first day at Union.</p>
+                </div>  
+            </div>
+        <script>
+            AOS.init();
+        </script>
+    
+</body>
+</html>
